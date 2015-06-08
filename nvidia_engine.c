43b521faeb17da9e13b5d1917df55e4a01314981
Johny <filip.jany@gmail.com>
2015-06-08 19:41:10 +0200

    cuda pre alpha

--- nvidia_engine.c
+++ nvidia_engine.c
@@ -17,33 +17,21 @@ __global__ int* getMooreNeighborhood(int* board_d, int cell)
 
 int* copyArrayToDevice(universe uni)
 {
-	int width = uni.width / BOARD_TYPE_LENGTH; //number of int's in array -> width
+	int width = uni.width; //number of int's in array -> width
 	int* board_d;
-	cudaMalloc((void**)&board_d, uni.height*width*sizeof(int))
-
-	cudaMemcpy(board_d, uni.board, uni.height*width*sizeof(int), cudaMemcpyHostToDevice);
+	cudaMalloc((void**)&board_d, uni.height*uni.width/sizeof(int));
+	
+	for(int i=0; i < uni.width; ++i)
+	{
+		cudaMemcpy(board_d+i*height/BOARD_TYPE_LENGTH, uni.board[i], uni.height/BOARD_TYPE_LENGTH, cudaMemcpyHostToDevice);
+	}
 	return board_d;
 }
 
 void copyArrayToHost(int* board_d, universe uni)
 {
-	int width = uni.width / BOARD_TYPE_LENGTH;
-	int* copied = (int*)calloc(uni.height*width*sizeof(int))
-	cudaMemcpy(copied, board_d, uni.height*width*sizeof(int), cudaMemcpyDeviceToHost);
-	//copy data from 1d array to 2d
-	int i = 0;
-	int j = 0;
-	int iter = 0;
-	while(iter < width*uni.height)
+	for(int i = 0; i < uni.width; ++i)
 	{
-		uni.board[i][j] = copied[iter];
-		++i;
-		++iter;
-		if(iter % width == 0)
-		{
-			i = 0;
-			j++;
-		}
+		cudaMemcpy(uni.board[i], board_d+i*height/BOARD_TYPE_LENGTH, uni.height/BOARD_TYPE_LENGTH, cudaMemcpyDeviceToHost);
 	}
-	free(copied);
 }
